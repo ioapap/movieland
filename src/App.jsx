de59be0199e6a6f3ec52c8fef31d3847559b7c7c
Ioannis <powerfulguy4774@gmail.com>
2023-06-20 13:30:25 +0200

    Added CSS files and updated the rest

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import {useEffect} from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
 
+const API_URL = "http://www.omdbapi.com?apikey=b92d9c5e";
+
+const movie1 = {
+    "Title": "Spiderman",
+    "Year": "1990",
+    "imdbID": "tt0100669",
+    "Type": "movie",
+    "Poster": "N/A"
+}
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch (`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect( () => {
+        searchMovies(`Spiderman`);
+    }, []);
+
     return (
-        <h1>App</h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input 
+                placeholder="Search for movies"
+                value="Superman"
+                onChange={() => {}}
+                />
+                <img 
+                src={SearchIcon}
+                alt="search"
+                onClick={() => {}}
+                />
+            </div>
+
+            <div className="container">
+                
+            </div>
+
+        </div>
     );
 }
 
