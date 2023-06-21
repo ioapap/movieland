a90c1b5e622f59f7116f09b4e8346bf7bc29b58b
Ioannis <powerfulguy4774@gmail.com>
2023-06-21 15:05:06 +0200

    Formatted the files and added the new MovieCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,48 @@
-import {useEffect} from "react";
+import { useEffect } from "react";
+import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com?apikey=b92d9c5e";
 
 const movie1 = {
-    "Title": "Spiderman",
-    "Year": "1990",
-    "imdbID": "tt0100669",
-    "Type": "movie",
-    "Poster": "N/A"
-}
+  Title: "Spiderman",
+  Year: "1990",
+  imdbID: "tt0100669",
+  Type: "movie",
+  Poster: "N/A",
+};
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
 
-    const searchMovies = async (title) => {
-        const response = await fetch (`${API_URL}&s=${title}`);
-        const data = await response.json();
+    console.log(data.Search);
+  };
 
-        console.log(data.Search);
-    }
+  useEffect(() => {
+    searchMovies(`Spiderman`);
+  }, []);
 
-    useEffect( () => {
-        searchMovies(`Spiderman`);
-    }, []);
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
 
-    return (
-        <div className="app">
-            <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
 
-            <div className="search">
-                <input 
-                placeholder="Search for movies"
-                value="Superman"
-                onChange={() => {}}
-                />
-                <img 
-                src={SearchIcon}
-                alt="search"
-                onClick={() => {}}
-                />
-            </div>
-
-            <div className="container">
-                <div className="movie">
-                    <div>
-                        <p>{movie1.Year}</p>
-                    </div>
-
-                    <div>
-                        <img src={movie1.Poster !== "N/A" ? movie1.Poster : "https://via.placeholder.com/400"} alt={movie1.Title}/>
-                    </div>
-
-                    <div>
-                        <span>{movie1.Type}</span>
-                        <h3>{movie1.Title}</h3>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    );
-}
+      <div className="container">
+        <MovieCard movie1={movie1} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
